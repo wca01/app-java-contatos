9242b465d6cf9ca8276463c5891d65c26181f0e4
wca <welbboy@live.com>
2023-11-03 14:43:30 -0300

    Iniciado aplicação Spring

---
+++ src/main/java/br/org/contatos/AppContatos/AppContatosApplication.java
@@ -0,0 +1,13 @@
+package br.org.contatos.AppContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/contatos/AppContatos/AppContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.contatos.AppContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
